063b028de91164ddd1c1514f9a4dd5a2909a0777
Renato Rocha <renatob.rocha@gmail.com>
2019-05-27 01:23:32 -0300

    first commit

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const crypto = require('crypto')
+const User = use('App/Models/User')
+const Mail = use('Mail')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('renatob.rocha@gmail.com', 'Renato Rocha')
+            .subject('Recuperação de senha')
+        }
+      )
+    } catch (error) {
+      console.log(error)
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Algo não deu certo, este e-mail existe?' } })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
